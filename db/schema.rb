c4f1e9bdf2c49ea0d136bd38a06124ddc8bf2bc6
Marcelo Soares Souza <marcelo@juntadados.org>
2014-10-05 13:14:56 -0300

    Implementando Permissões e Autorização.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141005142354) do
+ActiveRecord::Schema.define(version: 20141005153447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -162,6 +162,7 @@ ActiveRecord::Schema.define(version: 20141005142354) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
